4d669b6b0ca648c3fa9c9841ee08e9d03822863d
AbderrahmaneAmerhhi <Abderrahmane.amerrhi@gmail.com>
2023-06-20 18:31:47 +0100

    last

--- src/App.vue
+++ src/App.vue
@@ -15,96 +15,157 @@ import AmhVueTable from "amtablevuecomponenet";
 import "amtablevuecomponenet/dist/style.css";
 
 const state = reactive({
-  htmlCode: "<p>This is some HTML code</p>",
-  data: [
-    {
-      name: "ahmed",
-      age: 22,
-      aviable: 1,
-      date: new Date("12/05/2022"),
-      image: "https://via.placeholder.com/640x480.png/0033aa?text=dolore",
-    },
-    {
-      name: "hasan",
-      age: 33,
-      aviable: 1,
-      date: new Date("12/06/2022"),
-      image: "https://via.placeholder.com/640x480.png/0033aa?text=dolore",
-    },
-    {
-      name: "adam",
-      age: 17,
-      aviable: 0,
-      date: new Date("12/05/2024"),
-      image: "https://via.placeholder.com/640x480.png/0033aa?text=dolore",
-    },
-    {
-      name: "abdo",
-      age: 13,
-      aviable: 0,
-      date: new Date("12/05/2024"),
-    },
+     data: [{
+    name: 'ahmed',
+    age: 22,
+    aviable: 1,
+    date: new Date('12/05/2022'),
+    image: 'https://via.placeholder.com/640x480.png/0033aa?text=dolore'
+  },
+  {
+    name: 'hasan',
+    age: 33,
+    aviable: 1,
+    date: new Date('12/06/2022'),
+    image: 'https://via.placeholder.com/640x480.png/0033aa?text=dolore'
+  },
+  {
+    name: 'adam',
+    age: 17,
+    aviable: 0,
+    date: new Date('12/05/2024'),
+    image: 'https://via.placeholder.com/640x480.png/0033aa?text=dolore'
+
+  }
+    ,
+  {
+    name: 'abdo',
+    age: 13,
+    aviable: 0,
+    date: new Date('12/05/2024')
+
+  }
+    ,
+  {
+    name: 'mhend',
+    age: 14,
+    aviable: 0,
+    date: new Date('12/05/2024')
+
+  }
+    ,
+  {
+    name: 'samir',
+    age: 30,
+    aviable: 0,
+    date: new Date('12/05/2024')
+
+  }
+    ,
+  {
+    name: 'hsen',
+    age: 12,
+    aviable: 0,
+    date: new Date('12/05/2024')
+
+  }
+    ,
+  {
+    name: 'hamid',
+    age: 12,
+    aviable: 0,
+    date: new Date('12/05/2024')
+
+  }
+    ,
+  {
+    name: 'marwan',
+    age: 1,
+    aviable: 0,
+    date: new Date('12/05/2024')
+
+  }
+    ,
+  {
+    name: 'mostafa',
+    age: 1,
+    aviable: 0,
+    date: new Date('12/05/2024')
+
+  },
   ],
 
   columns: [
+
     {
-      title: "Name",
-      field: "name",
+      title: 'Name',
+      field: 'name',
+      visibility: false,
       EnableFilter: true,
       sortable: true,
       filterOptions: {
-        InputType: "text",
-        placeholder: "filter Bu Namee",
-      },
+        InputType: 'text',
+        placeholder: 'filter Bu Namee'
+      }
     },
     {
-      title: "AGE",
-      field: "age",
+      title: 'AGE',
+      field: 'age',
       EnableFilter: true,
       sortable: true,
       filterOptions: {
-        InputType: "text",
-        placeholder: "filter By age",
-      },
+        InputType: 'text',
+        placeholder: 'filter By age'
+      }
     },
     {
-      title: "Aviable",
-      field: "aviable",
+      title: 'Aviable',
+      field: 'aviable',
       EnableFilter: true,
       sortable: true,
       filterOptions: {
-        InputType: "select",
-        placeholder: "filter Bu aviablity",
+        InputType: 'select',
+        placeholder: 'filter Bu aviablity',
         filterSelectOptions: [
-          { title: "all", selected: true },
-          { title: "aviable", value: "1" },
-          { title: "non aviable", value: "0" },
-        ],
-      },
+          { title: 'all', selected: true },
+          { title: 'aviable', value: '1' },
+          { title: 'non aviable', value: '0' }
+        ]
+      }
     },
     {
-      title: "Date",
-      field: "date",
+      title: 'Date',
+      field: 'date',
       isDate: true,
-      dateMomentFormat: "L", // use momentjs formats
+      dateMomentFormat: 'L',  // use momentjs formats
       EnableFilter: true,
       filterOptions: {
-        InputType: "date",
-        placeholder: "filter Bu Date",
-      },
+        InputType: 'date',
+        placeholder: 'filter Bu Date',
+
+      }
     },
     {
-      title: "Image",
-      field: "image",
-      Cssclass: "imageClass",
+      title: 'Image',
+      field: 'image',
+      Cssclass: 'imageClass',
       isImage: true,
       EnableFilter: false,
+
     },
     {
-      title: "Actions",
+      title: 'Actions',
       isHtml: true,
       EnableFilter: false,
+
     },
+    {
+      title: 'Actions2',
+      isHtml: true,
+      EnableFilter: false,
+
+    },
+
   ],
   config: {
     EnableSearch: true,
