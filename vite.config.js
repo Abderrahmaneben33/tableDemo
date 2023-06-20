ca0a4be0ad056033ff1d29dc467f251f9eea532a
AbderrahmaneAmerhhi <Abderrahmane.amerrhi@gmail.com>
2023-06-20 12:55:59 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:"/dist",
+  base:"/tableDemo/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
