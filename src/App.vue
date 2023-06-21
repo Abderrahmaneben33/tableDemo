de66225fa19bf45bba1c8d5b02318c8fd00f5f3d
AbderrahmaneAmerhhi <Abderrahmane.amerrhi@gmail.com>
2023-06-21 13:56:31 +0100

    last

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 
 <template>
-  <AmhVueTable :data="state.data" :columns="state.columns" :config="state.config">
-    <template v-slot="{ column, row }">
-      <div v-if="column.title == 'Actions'">
-        <button @click="add(row.name)">add</button>
-      </div>
-    </template>
-  </AmhVueTable>
+  <div>
+
+    <AmhVueTable :data="state.data" :columns="state.columns" :config="state.config">
+      <template v-slot="{ column, row }">
+        <div v-if="column.title == 'Actions'">
+          <button @click="add(row.name)">add</button>
+        </div>
+      </template>
+    </AmhVueTable>
+  </div>
+  
 </template>
 <script setup>
 import { reactive } from "vue";
@@ -14,9 +18,12 @@ import AmhVueTable from "amtablevuecomponenet";
 // import css
 import "amtablevuecomponenet/dist/style.css";
 
+
+
 const state = reactive({
-     data: [{
-    name: 'ahmed',
+    data: [{
+      name: 'ahmed',
+    adresse:'casa',
     age: 22,
     aviable: 1,
     date: new Date('12/05/2022'),
@@ -24,6 +31,7 @@ const state = reactive({
   },
   {
     name: 'hasan',
+    adresse: 'essaouira',
     age: 33,
     aviable: 1,
     date: new Date('12/06/2022'),
@@ -31,6 +39,7 @@ const state = reactive({
   },
   {
     name: 'adam',
+    adresse: 'taza',
     age: 17,
     aviable: 0,
     date: new Date('12/05/2024'),
@@ -40,6 +49,7 @@ const state = reactive({
     ,
   {
     name: 'abdo',
+    adresse: 'chaoueen',
     age: 13,
     aviable: 0,
     date: new Date('12/05/2024')
@@ -48,6 +58,7 @@ const state = reactive({
     ,
   {
     name: 'mhend',
+    adresse: 'tantan',
     age: 14,
     aviable: 0,
     date: new Date('12/05/2024')
@@ -56,6 +67,7 @@ const state = reactive({
     ,
   {
     name: 'samir',
+    adresse: 'dakhla',
     age: 30,
     aviable: 0,
     date: new Date('12/05/2024')
@@ -64,6 +76,7 @@ const state = reactive({
     ,
   {
     name: 'hsen',
+    adresse: 'laayoun',
     age: 12,
     aviable: 0,
     date: new Date('12/05/2024')
@@ -72,6 +85,7 @@ const state = reactive({
     ,
   {
     name: 'hamid',
+    adresse: 'rabat',
     age: 12,
     aviable: 0,
     date: new Date('12/05/2024')
@@ -80,6 +94,7 @@ const state = reactive({
     ,
   {
     name: 'marwan',
+    adresse: 'safi',
     age: 1,
     aviable: 0,
     date: new Date('12/05/2024')
@@ -88,6 +103,7 @@ const state = reactive({
     ,
   {
     name: 'mostafa',
+    adresse: 'agadir',
     age: 1,
     aviable: 0,
     date: new Date('12/05/2024')
@@ -96,76 +112,76 @@ const state = reactive({
   ],
 
   columns: [
-
     {
-      title: 'Name',
-      field: 'name',
-      visibility: false,
+      title: "Name",
+      field: "name",
       EnableFilter: true,
       sortable: true,
       filterOptions: {
-        InputType: 'text',
-        placeholder: 'filter Bu Namee'
-      }
+        InputType: "text",
+        placeholder: "filter By Name",
+      },
     },
     {
-      title: 'AGE',
-      field: 'age',
+      title: "Adresse",
+      field: "adresse",
+      hidden:true,
       EnableFilter: true,
       sortable: true,
       filterOptions: {
-        InputType: 'text',
-        placeholder: 'filter By age'
-      }
+        InputType: "text",
+        placeholder: "filter By Name",
+      },
+    },
+    
+    {
+      title: "AGE",
+      field: "age",
+      EnableFilter: true,
+      sortable: true,
+      filterOptions: {
+        InputType: "text",
+        placeholder: "filter By age",
+      },
     },
     {
-      title: 'Aviable',
-      field: 'aviable',
+      title: "Aviable",
+      field: "aviable",
       EnableFilter: true,
       sortable: true,
       filterOptions: {
-        InputType: 'select',
-        placeholder: 'filter Bu aviablity',
+        InputType: "select",
+        placeholder: "filter By aviablity",
         filterSelectOptions: [
-          { title: 'all', selected: true },
-          { title: 'aviable', value: '1' },
-          { title: 'non aviable', value: '0' }
-        ]
-      }
+          { title: "all", selected: true },
+          { title: "aviable", value: "1" },
+          { title: "non aviable", value: "0" },
+        ],
+      },
     },
     {
-      title: 'Date',
-      field: 'date',
+      title: "Date",
+      field: "date",
       isDate: true,
-      dateMomentFormat: 'L',  // use momentjs formats
+      dateMomentFormat: "L", // use momentjs formats
       EnableFilter: true,
       filterOptions: {
-        InputType: 'date',
-        placeholder: 'filter Bu Date',
-
-      }
+        InputType: "date",
+        placeholder: "filter By Date",
+      },
     },
     {
-      title: 'Image',
-      field: 'image',
-      Cssclass: 'imageClass',
+      title: "Image",
+      field: "image",
+      Cssclass: "imageClass",
       isImage: true,
       EnableFilter: false,
-
     },
     {
-      title: 'Actions',
+      title: "Actions",
       isHtml: true,
       EnableFilter: false,
-
     },
-    {
-      title: 'Actions2',
-      isHtml: true,
-      EnableFilter: false,
-
-    },
-
   ],
   config: {
     EnableSearch: true,
@@ -180,4 +196,8 @@ const state = reactive({
     },
   },
 });
+
+const add = (name) => { 
+  console.log(name)
+}
 </script>
